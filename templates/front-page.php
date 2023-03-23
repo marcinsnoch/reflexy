37b249812af73bef6bd45d2e329725959b1896be
Marcin Snoch <marcin.msx@gmail.com>
2023-03-23 15:35:23 +0100

    Zmaiany

--- templates/front-page.php
+++ templates/front-page.php
@@ -1,18 +1,20 @@
-<div class="slice type5">
-    <?php if ($custom_query->have_posts()) : ?>
-        <article id="post-<?php the_ID(); ?>" <?php post_class(''); ?>>
-            <a href="./reflexy/"><h2 class="slice-title"><?php echo esc_html('REFLEXY'); ?></h2></a>
-            <div class="card-deck">
+<div class="slice type2">
+    <article id="post-<?php the_ID(); ?>" <?php post_class(''); ?>>
+        <a href="./reflexy/"><h2 class="slice-title"><?php echo esc_html('REFLEXY'); ?></h2></a>
+        <div class="card-deck">
+            <?php if ($custom_query->have_posts()) : ?>
                 <?php while ($custom_query->have_posts()) : $custom_query->the_post(); ?>
                     <div class="card">
                         <?php if (has_post_thumbnail()) : ?>
-                            <?php the_post_thumbnail('medium_large', array('class' => 'card-img')); ?>
+                            <?php the_post_thumbnail('medium_large', array('class' => 'card-img-top mb-1')); ?>
+                            <?php the_content(); ?>
+                        <?php else: ?>
+                            <?php the_content(); ?>
                         <?php endif; ?>
-                        <?php echo the_content(); ?>
                     </div>
                 <?php endwhile; ?>
-            </div>
-        </article>
-        <?php wp_reset_postdata(); ?>
-    <?php endif; ?>
+                <?php wp_reset_postdata(); ?>
+            <?php endif; ?>
+        </div>
+    </article>
 </div>
