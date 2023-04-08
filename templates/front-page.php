5a61a3a99df55149318c8582ab5ea6975d979dba
Marcin Snoch <marcin.msx@gmail.com>
2023-04-08 08:08:50 +0200

    Dodanie klasy "reflexy"

--- templates/front-page.php
+++ templates/front-page.php
@@ -1,4 +1,4 @@
-<div class="slice type2">
+<div class="slice type2 reflexy">
     <article id="post-<?php the_ID(); ?>" <?php post_class(''); ?>>
         <a href="./reflexy/"><h2 class="slice-title"><?php echo esc_html('REFLEXY'); ?></h2></a>
         <div class="card-deck">
@@ -6,7 +6,7 @@
                 <?php while ($custom_query->have_posts()) : $custom_query->the_post(); ?>
                     <div class="card">
                         <?php if (has_post_thumbnail()) : ?>
-                            <?php the_post_thumbnail('medium_large', array('class' => 'card-img-top mb-1')); ?>
+                            <?php the_post_thumbnail('medium_large', array('class' => 'card-img-top')); ?>
                             <?php the_content(); ?>
                         <?php else: ?>
                             <?php the_content(); ?>
