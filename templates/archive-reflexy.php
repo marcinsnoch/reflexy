c1ff3c58e22cd19673bffb89080cc4ea1c8354f8
Marcin Snoch <marcin.msx@gmail.com>
2024-06-16 09:59:48 +0200

    Zmiana klass CSS

--- templates/archive-reflexy.php
+++ templates/archive-reflexy.php
@@ -8,7 +8,7 @@ get_header();
 ?>
 <div class="container">
     <div class="row">
-        <div class="col-sm-12 col-md-8 offset-md-2">
+        <div class="reflexy col-sm-12 col-md-8 offset-md-2 mx-auto align-items-center">
             <?php if (have_posts()) : ?>
                 <?php while (have_posts()) : the_post(); ?>
                     <article id="post-<?php the_ID(); ?>" <?php post_class('mb-5 '); ?>>
